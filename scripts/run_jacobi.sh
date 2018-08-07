52f01ff275108b27fb4fffed833fb748b810c182
qoofyk <qoofyk@gmail.com>
2018-08-07 17:40:03 -0600

    init

---
+++ scripts/run_jacobi.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# First created: 2018 Aug 07
+# Last modified: 2018 Aug 07
+
+# Author: Yuankun Fu
+# email: fu121@purdue.edu
+
+NX_ARGS="--smp-workers=1 --throttle=dummy --instrumentation=tdg" ./release/jacobi
